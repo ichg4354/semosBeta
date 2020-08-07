8cf81b30b8cc54fcb03dedc5cf8f4b94c5f60196
user <60804817+ichg4354@users.noreply.github.com>
2020-08-07 15:04:21 +0900

    add css files and fix bug for MIME type css load

--- App.js
+++ App.js
@@ -3,10 +3,16 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
 import express from "express";
-import mainRouter from "./routers/mainRouter.js"
+import mainRouter from "./routers/mainRouter.js";
+
 const app = express();
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/css", express.static("css"));
+app.use("/image", express.static("image"));
 
-app.use('/',mainRouter)
-
+app.set("view engine", "pug");
+app.use("/", mainRouter);
 
 export default app;
--- routers/mainRouter.js
+++ routers/mainRouter.js
@@ -2,8 +2,8 @@ import express from "express";
 
 const mainRouter = express.Router();
 
-mainRouter.get("/", (req, res) => res.send("HOME"));
-mainRouter.get("/login", (req, res) => res.send("LOGIN"));
+mainRouter.get("/", (req, res) => res.render("home"));
+mainRouter.get("/login", (req, res) => res.render("login"));
 mainRouter.get("/myProfile/:id", (req, res) => res.send("MY PROFILE"));
 mainRouter.get("/market", (req, res) => res.send("MARKET"));
 mainRouter.get("/sports", (req, res) => res.send("SPORTS"));
