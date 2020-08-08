4594bd2aa79d21130c546a2f03d03f1fc8045726
user <60804817+ichg4354@users.noreply.github.com>
2020-08-08 16:30:27 +0900

    add password vertification to postRegisterUser

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,16 +1,20 @@
 import User from "../models/User.js";
 
 export const postRegisterUser = async (req, res) => {
-  const { name, email, age } = req.body;
-  try {
-    const newUser = await User.create({
-      name: name,
-      email: email,
-      age: age,
-    });
-    console.log(newUser);
-  } catch (e) {
-    console.log(e);
+  const { name, email, age, password1, password2 } = req.body;
+  if (password1 === password2) {
+    try {
+      const newUser = await User.create({
+        name: name,
+        email: email,
+        age: age,
+      });
+      console.log(newUser);
+    } catch (e) {
+      console.log(e);
+    }
+    res.redirect("/");
+  } else {
+    res.redirect("/register/user");
   }
-  res.redirect("/");
 };
